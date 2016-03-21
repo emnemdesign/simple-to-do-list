fd369a8e9dec779cf6a246b4fa7314d4a74a2153
mcle0341 <mcle0341@algonquinlive.com>
2016-03-21 17:03:17 -0400

    added toggle class

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 var $input = $('#todo-input');
 var $list = $('.list');
 
-$('form').on('submit', 'click', 'li', function (e) {
+$('form').on('submit', function (e) {
   var $li = $('<li>');
   e.preventDefault();
-  $li.html($input.val());
+  $li.html($input.val(''));
   $list.append($li);
+  
+});
+
+$('form').on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
 });

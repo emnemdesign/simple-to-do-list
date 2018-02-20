3279f1623b9fe17d6f3b94c510fb49ece85d47c2
mcle0341 <mcle0341@algonquinlive.com>
2018-02-19 23:55:34 -0500

    Update main.js
    added a second line of val('')

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ $('form').on('submit', function (e) {
   var $li = $('<li>');
 
   e.preventDefault();
-  $li.html($input.val(''));
+  $li.html($input.val());
+   $li.html($input.val(''));
   $list.append($li);
 });
 
